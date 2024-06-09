c0b09c57a7efbc0bf029ce80635aaf0bf05bb1dd
Omadar79 <99696526+Omadar79@users.noreply.github.com>
2024-06-09 07:50:08 -0700

    first commit

---
+++ game/src/game_main.c
@@ -0,0 +1,415 @@
+/*******************************************************************************************
+*
+*   Title: Herding Cats
+*   Descroption: My attempt at a simple cute cat game
+* 
+*   Programming: Dustin Taub
+*
+*   This game has been created using raylib (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// global variables shared between modules through screens.h
+GameScreen currentScreen = TITLE;
+Image logoImage;
+Texture2D mainTexture;
+Font font = { 0 };
+Music music = { 0 };
+Sound fxCoin = { 0 };
+
+//------------------------------------------------------------------------------
+// local variables
+static const int screenWidth = 800;
+static const int screenHeight = 600;
+
+// Required variables to manage screen transitions (fade-in, fade-out)
+static float transAlpha = 0.0f;
+static bool onTransition = false;
+static bool transFadeOut = false;
+static int transFromScreen = -1;
+static GameScreen transToScreen = UNKNOWN;
+
+//----------------------------------------------------------------------------------
+// local functions
+void ChangeToScreen(int screen);     // Change to screen, no transition effect
+void TransitionToScreen(int screen); // Request transition to next screen
+void UpdateTransition(void);         // Update transition effect
+void DrawTransition(void);           // Draw transition effect (full-screen rectangle)
+void UpdateDrawFrame(void);          // Update and draw one frame
+void InitializeGame(void);
+void InitializeAssets(void);
+void UnloadGame(void);
+
+
+// Main game entry point
+int main(void)
+{
+    InitWindow(screenWidth, screenHeight, "Herding Cats");
+
+    InitializeGame();
+
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
+    
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        UpdateDrawFrame();
+    }
+#endif
+
+    UnloadGame();
+  
+    return 0;
+}
+
+// Game Initialization
+void InitializeGame(void)
+{
+    InitAudioDevice();      // Initialize audio device
+
+    InitializeAssets();
+
+    SetMusicVolume(music, 1.0f);
+    PlayMusicStream(music);
+
+    // Setup and init first screen
+    currentScreen = TITLE;
+    InitLogoScreen();
+}
+
+// Asset Initialization
+void InitializeAssets(void)
+{
+    //  (assets that must be available in all screens, i.e. font)
+    font = LoadFont("resources/mecha.png");
+    logoImage = LoadImage("resources/herdingcatslogo.png");
+    music = LoadMusicStream("resources/ambient.ogg");
+    fxCoin = LoadSound("resources/coin.wav");
+
+    mainTexture = LoadTextureFromImage(logoImage);
+    
+}
+
+// De-Initialization and unload current screen data before closing
+void UnloadGame(void)
+{
+
+    switch (currentScreen)
+    {
+    case LOGO:
+        UnloadLogoScreen();
+        break;
+
+    case TITLE:
+        UnloadTitleScreen();
+        break;
+
+    case GAMEPLAY:
+        UnloadGameplayScreen();
+        break;
+
+    case ENDING:
+        UnloadEndingScreen();
+        break;
+
+    default:
+        break;
+    }
+
+    // Unload global data loaded
+    UnloadFont(font);
+    UnloadMusicStream(music);
+    UnloadSound(fxCoin);
+    UnloadImage(logoImage);   // Once image has been converted to texture and uploaded to VRAM, it can be unloaded from RAM
+    UnloadTexture(mainTexture);
+
+    CloseAudioDevice();     // Close audio context
+
+    CloseWindow();          // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+}
+
+// Change to next screen, no transition
+void ChangeToScreen(GameScreen screen)
+{
+    // Unload current screen
+    switch (currentScreen)
+    {
+        case LOGO: 
+            UnloadLogoScreen(); 
+            break;
+
+        case TITLE: 
+            UnloadTitleScreen(); 
+            break;
+
+        case GAMEPLAY: 
+            UnloadGameplayScreen(); 
+            break;
+
+        case ENDING: 
+            UnloadEndingScreen(); 
+            break;
+
+        default: 
+            break;
+    }
+
+    // Init next screen
+    switch (screen)
+    {
+        case LOGO:
+            InitLogoScreen();
+            break;
+
+        case TITLE:
+            InitTitleScreen();
+            break;
+
+        case GAMEPLAY: 
+            InitGameplayScreen(); 
+            break;
+
+        case ENDING: 
+            InitEndingScreen();
+            break;
+
+        default:
+            break;
+    }
+
+    currentScreen = screen;
+}
+
+// Request transition to next screen
+void TransitionToScreen(GameScreen screen)
+{
+    onTransition = true;
+    transFadeOut = false;
+    transFromScreen = currentScreen;
+    transToScreen = screen;
+    transAlpha = 0.0f;
+}
+
+// Update transition effect (fade-in, fade-out)
+void UpdateTransition(void)
+{
+    if (!transFadeOut)
+    {
+        transAlpha += 0.05f;
+
+        // NOTE: Due to float internal representation, condition jumps on 1.0f instead of 1.05f
+        // For that reason we compare against 1.01f, to avoid last frame loading stop
+        if (transAlpha > 1.01f)
+        {
+            transAlpha = 1.0f;
+
+            // Unload current screen
+            switch (transFromScreen)
+            {
+                case LOGO: 
+                    UnloadLogoScreen(); 
+                    break;
+
+                case TITLE: 
+                    UnloadTitleScreen(); 
+                    break;
+
+                case OPTIONS: 
+                    UnloadOptionsScreen(); 
+                    break;
+
+                case GAMEPLAY:
+                    UnloadGameplayScreen(); 
+                    break;
+
+                case ENDING: 
+                    UnloadEndingScreen(); 
+                    break;
+
+                default: 
+                    break;
+            }
+
+            // Load next screen
+            switch (transToScreen)
+            {
+                case LOGO: 
+                    InitLogoScreen(); 
+                    break;
+
+                case TITLE: 
+                    InitTitleScreen(); 
+                    break;
+
+                case GAMEPLAY: 
+                    InitGameplayScreen(); 
+                    break;
+
+                case ENDING: 
+                    InitEndingScreen(); 
+                    break;
+
+                default: 
+                    break;
+            }
+
+            currentScreen = transToScreen;
+
+            // Activate fade out effect to next loaded screen
+            transFadeOut = true;
+        }
+    }
+    else  // Transition fade out logic
+    {
+        transAlpha -= 0.02f;
+
+        if (transAlpha < -0.01f)
+        {
+            transAlpha = 0.0f;
+            transFadeOut = false;
+            onTransition = false;
+            transFromScreen = -1;
+            transToScreen = UNKNOWN;
+        }
+    }
+}
+
+// Draw transition effect (full-screen rectangle)
+void DrawTransition(void)
+{
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, transAlpha));
+}
+
+
+// Update and draw game frame
+void UpdateDrawFrame(void)
+{
+    // Music keeps playing between screens
+    UpdateMusicStream(music);       
+
+    //Make sure we are not in screen transition
+    if (!onTransition)
+    {
+        switch(currentScreen)
+        {
+            case LOGO:
+                UpdateLogoScreen();
+                if (FinishLogoScreen())
+                {
+                    TransitionToScreen(TITLE);
+                }
+                break;
+
+            case TITLE:
+                UpdateTitleScreen();
+
+                if (FinishTitleScreen() == 1)
+                {
+                    TransitionToScreen(OPTIONS);
+                }
+                else if (FinishTitleScreen() == 2)
+                {
+                    TransitionToScreen(GAMEPLAY);
+                }
+                break;
+
+            case OPTIONS:
+                UpdateOptionsScreen();
+
+                if (FinishOptionsScreen())
+                {
+                    TransitionToScreen(TITLE);
+                }
+                break;
+
+            case GAMEPLAY:
+                UpdateGameplayScreen();
+
+                if (FinishGameplayScreen() == 1)
+                {
+                    TransitionToScreen(ENDING);
+                }
+                //else if (FinishGameplayScreen() == 2) 
+                //  {
+                //      TransitionToScreen(TITLE);
+                //  }
+                break;
+
+            case ENDING:
+                UpdateEndingScreen();
+
+                if (FinishEndingScreen() == 1)
+                {
+                    TransitionToScreen(TITLE);
+                }
+                break;
+
+            default: 
+                break;
+        }
+    }
+    else  //if we are in screen transition lets update it
+    {
+        UpdateTransition();
+    }
+
+
+    //----------------------------------------------------------------------------------
+    // Main Draw Routine
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        //Draw the current screen/scene
+        switch(currentScreen)
+        {
+            case LOGO: 
+                DrawLogoScreen(); 
+                break;
+
+            case TITLE: 
+                DrawTitleScreen(); 
+                break;
+
+            case OPTIONS: 
+                DrawOptionsScreen(); 
+                break;
+
+            case GAMEPLAY: 
+                DrawGameplayScreen(); 
+                break;
+
+            case ENDING: 
+                DrawEndingScreen(); 
+                break;
+
+            default:
+                break;
+        }
+
+        // Draw full screen rectangle in front of everything
+        if (onTransition) 
+        {
+            DrawTransition();
+		}
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+}
---
+++ game/src/screen_title.c
@@ -0,0 +1,85 @@
+/**********************************************************************************************
+*
+*   raylib - Advance Game template
+*
+*   Title Screen Functions Definitions (Init, Update, Draw, Unload)
+*
+*   Copyright (c) 2014-2022 Ramon Santamaria (@raysan5)
+*
+*   This software is provided "as-is", without any express or implied warranty. In no event
+*   will the authors be held liable for any damages arising from the use of this software.
+*
+*   Permission is granted to anyone to use this software for any purpose, including commercial
+*   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+*
+*     1. The origin of this software must not be misrepresented; you must not claim that you
+*     wrote the original software. If you use this software in a product, an acknowledgment
+*     in the product documentation would be appreciated but is not required.
+*
+*     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+*     as being the original software.
+*
+*     3. This notice may not be removed or altered from any source distribution.
+*
+**********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"
+
+
+// local variables
+static int framesCounter = 0;
+static int finishScreen = 0;
+
+//----------------------------------------------------------------------------------
+// Title Screen Functions Definition
+//----------------------------------------------------------------------------------
+
+// Title Screen Initialization logic
+void InitTitleScreen(void)
+{
+      
+    framesCounter = 0;
+    finishScreen = 0;
+}
+
+// Title Screen Update logic
+void UpdateTitleScreen(void)
+{
+    // TODO: Update TITLE screen variables here!
+
+    // Press enter or tap to change to GAMEPLAY screen
+    if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+    {
+        //finishScreen = 1;   // OPTIONS
+        finishScreen = 2;   // GAMEPLAY
+        PlaySound(fxCoin);
+    }
+}
+
+// Title Screen Draw logic
+void DrawTitleScreen(void)
+{
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLUE);
+    Vector2 pos = { 20, 10 };
+    DrawTextEx(font, "Herding Cats", pos, font.baseSize*3.0f, 4, DARKGREEN);
+    DrawText("PRESS ENTER or TAP to JUMP to Start Game", 120, 400, 20, DARKGREEN);
+
+    int mainLogoX;// = 10;//((int)GetScreenWidth() - mainLogo.width) / 2;
+    int mainLogoY;// = 220;
+    mainLogoX = GetScreenWidth() / 2 - mainTexture.width / 2;
+    mainLogoY = GetScreenHeight() / 2 - mainTexture.height / 2;
+    DrawTexture(mainTexture, mainLogoX, mainLogoY, WHITE);
+}
+
+// Title Screen Unload logic
+void UnloadTitleScreen(void)
+{
+    // TODO: Unload TITLE screen variables here!
+}
+
+// Title Screen should finish?
+int FinishTitleScreen(void)
+{
+    return finishScreen;
+}
